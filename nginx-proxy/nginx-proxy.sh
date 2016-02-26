cb0ce0c6eae6ecf813a066ee31b8d5f06d1ae2f5
Martin Schnabel <mcnilz@gmail.com>
2016-02-26 13:13:04 +0100

    nginx-proxy

---
+++ nginx-proxy/nginx-proxy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker run \
+	-d \
+	--restart=always \
+	--name nginx-proxy \
+	--volume /var/run/docker.sock:/tmp/docker.sock:ro \
+	--volume nginx-proxy-certs:/etc/nginx/certs:ro \
+	--volume nginx-proxy-vhost:/etc/nginx/vhost.d \
+	--volume nginx-proxy-html:/usr/share/nginx/html \
+	-p 80:80 \
+	-p 443:443 \
+	$@ \
+	jwilder/nginx-proxy
