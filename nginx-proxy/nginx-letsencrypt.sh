5747838bcfd65cf5b2465227cffdb5e640767d06
Martin Schnabel <mcnilz@gmail.com>
2016-03-02 16:23:03 +0100

    fixes

--- nginx-proxy/nginx-letsencrypt.sh
+++ nginx-proxy/nginx-letsencrypt.sh
@@ -3,11 +3,12 @@
 docker run \
 	-d \
 	--restart=always \
-	--name nginx-proxy \
-	--volume /var/run/docker.sock:/tmp/docker.sock:ro \
+	--name nginx-letsencrypt \
+	--volume /var/run/docker.sock:/var/run/docker.sock:ro \
 	--volume nginx-proxy-certs:/etc/nginx/certs:rw \
 	--volume nginx-proxy-vhost:/etc/nginx/vhost.d \
 	--volume nginx-proxy-html:/usr/share/nginx/html \
+	-e NGINX_PROXY_CONTAINER=nginx-proxy \
 	-p 80:80 \
 	-p 443:443 \
 	$@ \
