d475d2f1e07db7668ad3e62e4d8e07a494bc1cc3
Martin Schnabel <mcnilz@gmail.com>
2016-03-19 15:37:51 +0100

    rancher start update

--- rancher-local/rancher.sh
+++ rancher-local/rancher.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -e
 
-IP=`ifconfig docker0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+IP=`sudo ifconfig docker0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+
+docker pull rancher/server
 
 docker run -d \
 	--name rancher-server \
 	--restart=always \
 	-p ${IP}:8999:8080 \
+	-v rancher-mysql-data:/var/lib/mysql \
 	rancher/server
 
 echo "http://${IP}:8999"
