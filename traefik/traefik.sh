0bb50d58623954e5ecd52ebb8f40a95665e0ab83
Martin Schnabel <mcnilz@gmail.com>
2016-04-10 14:06:54 +0200

    treafik updated

--- traefik/traefik.sh
+++ traefik/traefik.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+docker pull treafik
+
 docker run -d -p 127.0.0.1:8080:8080 -p 127.0.0.1:80:80 \
 	-v /var/run/docker.sock:/var/run/docker.sock:ro \
 	-v $PWD/traefik.toml:/traefik.toml:ro \
 	--name traefik \
 	--restart always \
-	emilevauge/traefik
+	traefik
