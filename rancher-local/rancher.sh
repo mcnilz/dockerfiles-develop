e17349dcd6b3ef633cd030ee54714bef7e4b28b9
Martin Schnabel <mcnilz@gmail.com>
2016-05-03 07:52:08 +0200

    rancher script updated

--- rancher-local/rancher.sh
+++ rancher-local/rancher.sh
@@ -1,14 +1,43 @@
 #!/bin/bash -e
 
+TAG=stable
+
+if [ -n "$1" ]; then
+	TAG=$1
+fi
+
+IMAGE="rancher/server:${TAG}"
+CONTAINER="rancher-server"
+
+echo "Image: $IMAGE"
+
 IP=`sudo ifconfig docker0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 
-docker pull rancher/server
+docker pull $IMAGE
+
+set +e
+RUNNING=$(docker inspect --format="{{ .State.Running }}" $CONTAINER 2> /dev/null)
+set -e
+
+if [ "$RUNNING" == "true" ]; then
+	echo Stopping container...
+	docker stop -t 1 ${CONTAINER}
+fi
+
+set +e
+RUNNING=$(docker inspect --format="{{ .State.Running }}" $CONTAINER 2> /dev/null)
+set -e
+
+if [ "$RUNNING" == "false" ]; then
+	echo Removing container...
+	docker rm ${CONTAINER}
+fi
 
 docker run -d \
-	--name rancher-server \
+	--name $CONTAINER \
 	--restart=always \
 	-p ${IP}:8999:8080 \
 	-v rancher-mysql-data:/var/lib/mysql \
-	rancher/server
+	$IMAGE
 
 echo "http://${IP}:8999"
